5af8417066e5b866c55be832e3bcd3edc8a1b6c5
whateverusername <dani@gmail.com>
2021-04-22 08:29:47 +0300

    first commitft

--- solution.py
+++ solution.py
@@ -184,6 +184,7 @@ def get_route(hostname):
                     tracelist1.append(addr[0])
                     #tracelist1.append(addr[0])
                     tracelist2.append(tracelist1)
+                    return tracelist2
                     #You should add your responses to your lists here and return your list if your destination IP is met
                     #Fill in end
                 else:
